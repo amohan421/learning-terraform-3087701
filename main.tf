2d873421148e6860ebf35cf6e699d946840b2451
amohan421 <125665849+amohan421@users.noreply.github.com>
2023-02-19 00:06:14 +0530

    lb4

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ module "blog_sg" {
   version = "4.17.1"
   
   name    = "blog"
-  vpc_id  =aws_default_vpc.default.id
+  vpc_id  =module.vpc.public_subnets
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
