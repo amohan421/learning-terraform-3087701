d6e2f4ac719f3de9a1632e68ed4e447b0635c368
amohan421 <125665849+amohan421@users.noreply.github.com>
2023-02-18 19:02:49 +0530

    use module for sec group3

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "web" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = blog_new
+  name    = "blog_new"
   
   vpc_id=data.aws_vpc.default.id
 
