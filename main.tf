4a243c09c1a363e31305ac7cac3638227a71bf64
amohan421 <125665849+amohan421@users.noreply.github.com>
2023-02-18 20:24:24 +0530

    vpc12

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ module "blog_sg" {
   version = "4.17.1"
   
   name    = "blog"
-  vpc_id  =module.vpc.public_subnets[1]
+  vpc_id  =module.vpc.public_subnets[2]
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks   = ["0.0.0.0/0"]
